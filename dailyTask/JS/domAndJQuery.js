aa9bff20020043baa2ee4e6653a3808fd9fdbe65
Promo656 <rank2007@mail.ru>
2020-05-16 23:53:04 +0300

    commit

---
+++ dailyTask/JS/domAndJQuery.js
@@ -0,0 +1,7 @@
+/* let headingElement= document.getElementById("test")
+console.log(headingElement.innerHTML);
+let newHeadingText=prompt("Enter the new title")
+headingElement.innerHTML=newHeadingText
+console.log(headingElement.innerHTML); */
+let newHeadingText=prompt("Enter the new title")
+$("#main-heading").text(newHeadingText)
