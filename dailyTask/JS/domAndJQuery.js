e2be0e9cf83fbae59b5635ba4d4037b6ccc3d545
Promo656 <rank2007@mail.ru>
2020-05-17 21:32:25 +0300

    commit

--- dailyTask/JS/domAndJQuery.js
+++ dailyTask/JS/domAndJQuery.js
@@ -2,11 +2,34 @@
 console.log(headingElement.innerHTML);
 let newHeadingText=prompt("Enter the new title")
 headingElement.innerHTML=newHeadingText
-console.log(headingElement.innerHTML); */
-let newHeadingText=prompt("Enter the new title")
+console.log(headingElement.innerHTML); 
+console.log("----------------------------------------------");*/
+
+/* let newHeadingText=prompt("Enter the new title")
 $("#main-heading").text(newHeadingText)
 $("body").append("<p> Это новый абзац</p>")
 for (let i=0; i<3; i++) {
     let hobby=prompt("What kind of hobby")
     $("body").append("<p>"+ hobby+ "</p>")
+} 
+console.log("----------------------------------------------")*/
+
+/* $("h1").text("This text soon disapear").fadeOut(3000).fadeIn(3000).slideUp(3000).slideDown(3000)
+$("h2").slideUp(3000).slideDown(3000) 
+for (let i=0; i<5; i++) {
+    $("h1").fadeOut(3000).fadeIn(3000)
 }
+$("h1").fadeOut(2000)
+$("h1").fadeIn(2000)*/
+
+let arr1 = [
+    "Vova",
+    "Dima",
+    "Egor"
+]
+for (let i = 0; i < arr1.length; i++) {
+    $("body").append("<p>" + arr1[i] + " the BEST"+ "</p>")
+}
+$("h1").text("Hello, fiends")
+$("p").hide()
+$("p").fadeIn(3000)
