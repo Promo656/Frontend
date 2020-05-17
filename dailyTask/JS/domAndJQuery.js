834aad6b1ce37b5539e322c071aa7d6bd0cb3f72
Promo656 <rank2007@mail.ru>
2020-05-17 17:36:02 +0300

    commit

--- dailyTask/JS/domAndJQuery.js
+++ dailyTask/JS/domAndJQuery.js
@@ -5,3 +5,8 @@ headingElement.innerHTML=newHeadingText
 console.log(headingElement.innerHTML); */
 let newHeadingText=prompt("Enter the new title")
 $("#main-heading").text(newHeadingText)
+$("body").append("<p> Это новый абзац</p>")
+for (let i=0; i<3; i++) {
+    let hobby=prompt("What kind of hobby")
+    $("body").append("<p>"+ hobby+ "</p>")
+}
