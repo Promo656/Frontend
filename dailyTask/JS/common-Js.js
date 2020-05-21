601dad66759fff42120cbec8869b6ea039291918
Promo656 <rank2007@mail.ru>
2020-05-22 00:06:35 +0300

    common

--- dailyTask/JS/common-Js.js
+++ dailyTask/JS/common-Js.js
@@ -452,7 +452,7 @@ var double=function (ch) {
 
     alert(func2()+func1()) */
 
-    let func1=function(){
+/*     let func1=function(){
         return 4*3
     }
 
@@ -464,4 +464,24 @@ var double=function (ch) {
         return n1+n2
     }
 
-    alert(sum(func1(),func2()))
+    alert(sum(func1(),func2())) */
+    
+    function  calculateAge(date1, date2) {
+
+
+        if(date2-date1==1){
+         return "You are "+(date2-date1)+" year old."
+        } else if (date1-date2==1) {
+        return "You will be born in "+(date1-date2)+" year."
+        } else if (date1==date2){
+        return "You were born this very year!"
+        } else if (date1>date2){
+        return "You will be born in "+(date1-date2)+" years."
+        } else if (date1<date2){
+        return "You are "+(date2-date1)+" years old."
+        }
+        
+        
+        
+        }
+      calculateAge();
