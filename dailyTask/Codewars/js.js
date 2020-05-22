1c098c3f870a1dd18bd4e6ae480c0465507e5e28
Promo656 <rank2007@mail.ru>
2020-05-22 23:14:50 +0300

    commit

--- dailyTask/Codewars/js.js
+++ dailyTask/Codewars/js.js
@@ -1,6 +1,4 @@
 function calculateAge(date1, date2) {
-
-
     if (date2 - date1 == 1) {
         return "You are " + (date2 - date1) + " year old."
     } else if (date1 - date2 == 1) {
@@ -14,3 +12,8 @@ function calculateAge(date1, date2) {
     }
 }
 calculateAge(1994,2020);
+
+function greet(){
+    return "hello world!"
+    }
+    greet()
