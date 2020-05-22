2d864a878dd8f9b66957c071e4239dd55db56945
Promo656 <rank2007@mail.ru>
2020-05-22 23:11:23 +0300

    commit

--- dailyTask/JS/common-Js.js
+++ dailyTask/JS/common-Js.js
@@ -465,23 +465,5 @@ var double=function (ch) {
     }
 
     alert(sum(func1(),func2())) */
-    
-    function  calculateAge(date1, date2) {
 
 
-        if(date2-date1==1){
-         return "You are "+(date2-date1)+" year old."
-        } else if (date1-date2==1) {
-        return "You will be born in "+(date1-date2)+" year."
-        } else if (date1==date2){
-        return "You were born this very year!"
-        } else if (date1>date2){
-        return "You will be born in "+(date1-date2)+" years."
-        } else if (date1<date2){
-        return "You are "+(date2-date1)+" years old."
-        }
-        
-        
-        
-        }
-      calculateAge();
