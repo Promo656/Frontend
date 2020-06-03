3fcb00d04e2ae3c05f90b6346b21f0a2803a9d6c
Promo656 <rank2007@mail.ru>
2020-06-03 22:07:42 +0300

    commit

--- dailyTask/Codewars/js.js
+++ dailyTask/Codewars/js.js
@@ -200,3 +200,12 @@ numberToString(76) */
 };
 isReallyNaN(4) */
 /*----------------------------------------------------------------------------- */
+
+function squareSum(numbers) {
+    let sum = 0
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i] * numbers[i]
+    }
+    return sum
+}
+squareSum([1,2,2])
