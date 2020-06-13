f4a6d782602eef91a60924f7b72cd4daddfaa840
Promo656 <rank2007@mail.ru>
2020-06-13 23:45:45 +0300

    commit

--- react-project-on-typescript/my-app/src/index.tsx
+++ react-project-on-typescript/my-app/src/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import App from "./App";
 
 ReactDOM.render(
-  <div>Hello</div>,
-  document.getElementById('root')
+  <App/>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
