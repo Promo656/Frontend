07b1087a96d3f54bacdca813828e75b870e5decf
Promo656 <rank2007@mail.ru>
2020-06-20 20:37:55 +0000

    commit

--- react-project/my-first-app/src/App.js
+++ react-project/my-first-app/src/App.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import './App.css';
-import Technologies from './Technologies'
-import Header from './header'
-import Footer from './Footer'
-
 
 function App() {
   return (
     <div>
-      <Header />
-      <Technologies />
-      <Footer />
     </div>
   )
 }
