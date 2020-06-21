9a05dd92325297e6e325feb250095819d30d5c27
Promo656 <rank2007@mail.ru>
2020-06-21 23:10:52 +0300

    commit

--- react-project/my-first-app/src/App.js
+++ react-project/my-first-app/src/App.js
@@ -3,11 +3,11 @@ import './App.css'
 
 function App() {
     return (
-        <div className='container'>
-            <header className='header'>
+        <div className="container">
+            <header>
                 <img src="https://cdn.pixabay.com/photo/2014/04/02/17/01/logo-307673_1280.png" alt=""/>
             </header>
-            <nav className='nav'>
+            <nav>
                 <ul className='ul'>
                     <li><a href="#">Profile</a></li>
                     <li><a href="#">Messages</a></li>
@@ -16,25 +16,25 @@ function App() {
                     <li><a href="#">Settings</a></li>
                 </ul>
             </nav>
-            <main className='content'>
-                <div className='top-back'>
-                    <img
-                        src="https://avatars.mds.yandex.net/get-pdb/1773776/61bb1562-80ed-4c77-951d-8c929fb0308f/s1200?webp=false"
-                        alt=""/>
-                </div>
-                <div className='profiles-logo'>
-                    <img
-                        src="https://avatars.mds.yandex.net/get-pdb/750514/dd070fae-d858-41e6-a955-01b8a68b79a2/s1200?webp=false"
-                        alt=""/>
-                </div>
-                <div className='about'>
-                    <ul>
-                        <li>Date of Birth:</li>
-                        <li>City:</li>
-                        <li>Education:</li>
-                        <li>Website:</li>
-                    </ul>
-                </div>
+            <main>
+                <article>
+                    <section className="personBackgroundPhoto">
+                    </section>
+                    <section className="aboutPerson">
+                        <div className="photo">
+                        </div>
+                        <div className="description">
+                            <ul>
+                                <li className="name">Egor P.</li>
+                                <li>Date of Birth: 1994</li>
+                                <li>City: Moscow</li>
+                                <li>Education: MSU</li>
+                                <li>Website: github</li>
+
+                            </ul>
+                        </div>
+                    </section>
+                </article>
             </main>
         </div>
     )
