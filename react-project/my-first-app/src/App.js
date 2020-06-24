b647f4fbfdcfe5f73f1117913ed17f98a79c613d
Promo656 <rank2007@mail.ru>
2020-06-24 13:00:19 +0300

    commit

--- react-project/my-first-app/src/App.js
+++ react-project/my-first-app/src/App.js
@@ -35,6 +35,18 @@ function App() {
                         </div>
                     </section>
                 </article>
+                <article className='post'>
+                    <h1>My post</h1>
+                    <section className='newPost'>
+                        <input className='placeholder' type="text" placeholder='your news...'/>
+                        <input className='button' type="submit"/>
+                    </section>
+                    <hr className='hr'/>
+                    <section className='oldPost'>
+                        <div className='circle'></div>
+                        <div className='oldPostText'>Hello,guys! I'am here!</div>
+                    </section>
+                </article>
             </main>
         </div>
     )
